cbeef089498869b0041758c3dc20a6781edeaf1c
Madeline Rombes <madelinerombes@gmail.com>
2019-03-25 20:41:43 -0400

    added logo

--- etsy-app/src/components/Header.js
+++ etsy-app/src/components/Header.js
@@ -3,14 +3,12 @@ import logo from '../logo.png'
 
 export default class Header extends Component {
 
-
-
   render() {
     return (
       <div className="header">
         <img className="logo" alt="shop-logo" src={logo} />
         <div className="right-header">
-          <h1>The Etsy App</h1>
+          <h1>EtsyBoard</h1>
           <input onChange={(e) => this.props.handleInput(e)} type="text" name="search" input={this.props.keyword} />
           <button onClick={(e) => this.props.handleClick(e)}>Search</button>
         </div>
